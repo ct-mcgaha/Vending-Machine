ffcbf8ffa91519e5344ad2109f4096b3a6d4623b
[A[A[A[B[B[B[B[B[BConnor McGaha <ctmcgaha@gmail.com>
2020-11-22 11:06:43 -0500

    CT Tests Push

--- java/src/test/java/com/techelevator/VendingMachineCLITest.java
+++ java/src/test/java/com/techelevator/VendingMachineCLITest.java
@@ -9,10 +9,37 @@ public class VendingMachineCLITest {
 
 	@Test
 	public void MAIN_MENU_option_1_selects_DISPLAY_ITEMS() {
-		VendingMachineCLI vend = new VendingMachineCLI();
-		String choice = "1";
-		List<VendingMachine> stringList = new ArrayList<>();
+		VendingMachineCLI display = new VendingMachineCLI();
 		
+		
+		List<VendingMachine> expected = display.readFile();
+		List<VendingMachine> actual = 
+	}
+	
+	@Test
+	public void feed_5_dollars_returns_500() {
+		VendingMachineCLI feedMoney = new VendingMachineCLI();
+		
+	}
+	
+	@Test
+	public void select_a1_returns_Potato_Crisps() {
+		VendingMachineCLI potatoCrisps = new VendingMachineCLI();
+		
+	}
+	
+	@Test
+	public void select_b1_returns_PotatoCrisps_false() {
+		
+	}
+	
+	@Test 
+	public void finish_transaction_returns_change() {
+		
+	}
+	
+	@Test
+	public void finish_transaction_returns_coins() {
 		
 	}
 
